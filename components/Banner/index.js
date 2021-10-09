e73e245b0d89623129eee9decfb6e56034878988
Anthony Cavuoti <anthonycavuoti@gmail.com>
2021-10-08 17:06:39 -0700

    re oranged banner component items

--- components/Banner/index.js
+++ components/Banner/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+import './style.scss';
 
 const Banner = () => {
+   let isMobile = useMediaQuery({ query: '(max-width: 480px)' });
+
    return (
-      <section className='banner-one' id='banner'>
+      <section className='banner-one-container banner-one' id='banner'>
          <span className='banner-one__shape-1'></span>
          <span className='banner-one__shape-2'></span>
          <span className='banner-one__shape-3'></span>
          <span className='banner-one__shape-4'></span>
          <div className='container'>
             <div className='banner-one__moc'>
+               {isMobile ? <Title /> : <></>}
                <img
                   src='/assets/images/mocs/banner-moc-1-1.png'
                   alt='Awesome Image'
@@ -17,11 +22,7 @@ const Banner = () => {
             <div className='row'>
                <div className='col-xl-6 col-lg-8'>
                   <div className='banner-one__content'>
-                     <h3 className='banner-one__title'>
-                        We Make Building your
-                        <span> Website </span>
-                        Fun and Easy
-                     </h3>
+                     {!isMobile ? <Title /> : <></>}
                      <p className='banner-one__text'>
                         Do you need a website with guaranteed great quality?{' '}
                         <br /> Are there specific requirements you need in your
@@ -37,4 +38,14 @@ const Banner = () => {
       </section>
    );
 };
+
+function Title() {
+   return (
+      <h3 className='title banner-one__title'>
+         We Make Building your
+         <span> Website </span>
+         Fun and Easy
+      </h3>
+   );
+}
 export default Banner;
