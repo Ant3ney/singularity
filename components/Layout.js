108e1e66ebca67c47a783fe650d828c2cf784804
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-04-13 10:43:29 -0700

    Add add tracking code

--- components/Layout.js
+++ components/Layout.js
@@ -3,50 +3,33 @@ import Head from 'next/head';
 import warningErrorFilter from '../config/warningErrorFilter';
 
 const Layout = props => {
-   return (
-      <div>
-         <Head>
-            <title>{props.pageTitle}</title>
-            <meta charSet='UTF-8' />
-            <meta
-               name='viewport'
-               content='width=device-width, initial-scale=1.0'
-            />
-            <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
-            <link
-               rel='apple-touch-icon'
-               sizes='180x180'
-               href='/assets/images/favicons/apple-touch-icon.png'
-            />
-            <link
-               rel='icon'
-               type='image/png'
-               sizes='32x32'
-               href='/assets/images/favicons/favicon-32x32.png'
-            />
-            <link
-               rel='icon'
-               type='image/png'
-               sizes='16x16'
-               href='/assets/images/favicons/favicon-16x16.png'
-            />
-            <link
-               href='https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&display=swap'
-               rel='stylesheet'
-            />
-            <link rel='stylesheet' href='/assets/css/animate.min.css' />
-            <link rel='stylesheet' href='/assets/css/bootstrap.min.css' />
-            <link rel='stylesheet' href='/assets/css/font-awesome.min.css' />
-            <link
-               rel='stylesheet'
-               href='/assets/plugins/dimon-icons/style.css'
-            />
-            <link rel='stylesheet' href='/assets/css/style.css' />
-            <link rel='stylesheet' href='/assets/css/responsive.css' />
-         </Head>
+	return (
+		<div>
+			<Head>
+				<title>{props.pageTitle}</title>
+				<meta charSet='UTF-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+				<link rel='apple-touch-icon' sizes='180x180' href='/assets/images/favicons/apple-touch-icon.png' />
+				<link rel='icon' type='image/png' sizes='32x32' href='/assets/images/favicons/favicon-32x32.png' />
+				<link rel='icon' type='image/png' sizes='16x16' href='/assets/images/favicons/favicon-16x16.png' />
+				<link
+					href='https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&display=swap'
+					rel='stylesheet'
+				/>
+				<link rel='stylesheet' href='/assets/css/animate.min.css' />
+				<link rel='stylesheet' href='/assets/css/bootstrap.min.css' />
+				<link rel='stylesheet' href='/assets/css/font-awesome.min.css' />
+				<link rel='stylesheet' href='/assets/plugins/dimon-icons/style.css' />
+				<link rel='stylesheet' href='/assets/css/style.css' />
+				<link rel='stylesheet' href='/assets/css/responsive.css' />
 
-         <div className='page-wrapper'>{props.children}</div>
-      </div>
-   );
+				{/* Global site tag (gtag.js) - Google Ads: 1005517184  */}
+				<script async src='https://www.googletagmanager.com/gtag/js?id=AW-1005517184'></script>
+			</Head>
+
+			<div className='page-wrapper'>{props.children}</div>
+		</div>
+	);
 };
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import '../node_modules/react-modal-video/scss/modal-video.scss';
 import warningErrorFilter from '../config/warningErrorFilter';
+import { useEffect } from 'react';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-   warningErrorFilter();
-   return <Component {...pageProps} suppressHydrationWarning={true} />;
+	useEffect(() => {
+		let dataLayer = window.dataLayer;
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer ? dataLayer.push(arguments) : console.log('data layer is not defined');
+		}
+		gtag('js', new Date());
+		gtag('config', 'AW-1005517184');
+
+		/* Event snippet for Website lead conversion page */
+
+		gtag('event', 'conversion', { send_to: 'AW-1005517184/ChCeCO_NwrIDEIDzu98D' });
+	}, []);
+	warningErrorFilter();
+	return <Component {...pageProps} suppressHydrationWarning={true} />;
 }
