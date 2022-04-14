75d7b4e7353cdcc4dc65da91065c3eee6ccfda64
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-04-13 23:56:23 -0700

    Add logging

--- API/getProductsData.js
+++ API/getProductsData.js
@@ -1,27 +1,28 @@
 import client from '../config/sanityClient';
 
 export default function getProductsData() {
-   /* Note, route must be properly formated before calling this function */
-   /* expecting '/route' */
+	/* Note, route must be properly formated before calling this function */
+	/* expecting '/route' */
 
-   //TODO when this is all working, make newProductSchema just porduct
-   const query = `*[_type == "product"]{ ...,}`;
-   return new Promise((resolve, reject) => {
-      client
-         .fetch(query)
-         .then(pages => {
-            if (pages.length <= 0) {
-               console.log(`pages.length  <= 0`);
-               resolve({
-                  hasFailed: true,
-                  message: 'Query returned no results',
-               });
-            }
-            resolve(pages);
-         })
-         .catch(err => {
-            console.log(err);
-            resolve({ hasFailed: true, error: err });
-         });
-   });
+	//TODO when this is all working, make newProductSchema just porduct
+	const query = `*[_type == "product"]{ ...,}`;
+	return new Promise((resolve, reject) => {
+		client
+			.fetch(query)
+			.then(pages => {
+				if (pages.length <= 0) {
+					console.log(`pages.length  <= 0`);
+					resolve({
+						hasFailed: true,
+						message: 'Query returned no results',
+					});
+				}
+				console.log('pages', pages);
+				resolve(pages);
+			})
+			.catch(err => {
+				console.log(err);
+				resolve({ hasFailed: true, error: err });
+			});
+	});
 }
