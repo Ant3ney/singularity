2976fae6fe8cce4f498675723531b4eb99c21a93
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-06-13 17:00:11 -0700

    Updated build

--- schemas/LandingScreenSection/index.js
+++ schemas/LandingScreenSection/index.js
@@ -1,24 +1,29 @@
 export default {
-   title: 'landing Screen',
-   name: 'landingScreen',
-   type: 'document',
-   fields: [
-      {
-         title: 'Display Image',
-         name: 'displayImage',
-         type: 'image',
-      },
-      {
-         title: 'Title',
-         name: 'title',
-         type: 'boldsBreaksAndSpans',
-      },
-   ],
-   preview: {
-      prepare() {
-         return {
-            title: `Landing Screen`,
-         };
-      },
-   },
+	title: 'landing Screen',
+	name: 'landingScreen',
+	type: 'document',
+	fields: [
+		{
+			title: 'Display Image',
+			name: 'displayImage',
+			type: 'image',
+		},
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'boldsBreaksAndSpans',
+		},
+		{
+			title: 'Subtitle',
+			name: 'subtitle',
+			type: 'boldsBreaksAndSpans',
+		},
+	],
+	preview: {
+		prepare() {
+			return {
+				title: `Landing Screen`,
+			};
+		},
+	},
 };
