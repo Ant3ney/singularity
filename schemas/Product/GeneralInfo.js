25ae94b85fcb729415406d1fac0fcafa8e53af69
Anthony Cavuoti <anthonycavuoti@gmail.com>
2022-09-29 12:25:32 -0700

    Install packages and add todos for new download option for products

--- schemas/Product/GeneralInfo.js
+++ schemas/Product/GeneralInfo.js
@@ -1,58 +1,56 @@
 export default {
-   title: 'Product',
-   name: 'generalProductInfo',
-   type: 'document',
-   fields: [
-      {
-         title: 'Title',
-         name: 'title',
-         type: 'string',
-      },
-      {
-         title: 'Slug',
-         name: 'slug',
-         type: 'slug',
-         options: {
-            source: `generalInfo`,
-            slugify: input => {
-               return input
-                  ? input.title.toLowerCase().replace(/\s+/g, '-').slice(0, 200)
-                  : '';
-            },
-         },
-      },
-      {
-         title: 'Short Description',
-         name: 'shortDescription',
-         type: 'string',
-      },
-      {
-         title: 'Price',
-         name: 'price',
-         type: 'number',
-      },
-      {
-         title: 'Thumbnail',
-         name: 'thumbnail',
-         type: 'image',
-      },
-      {
-         title: 'Form Addon Message',
-         name: 'pluginMessage',
-         type: 'string',
-      },
-      { title: 'Priority', name: 'priority', type: 'number' },
-   ],
-   preview: {
-      select: {
-         title: 'title',
-      },
-      prepare(selection) {
-         return {
-            title: `Product - "${
-               !selection.title ? 'Unset' : selection.title
-            }"`,
-         };
-      },
-   },
+	title: 'Product',
+	name: 'generalProductInfo',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string',
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: `generalInfo`,
+				slugify: input => {
+					return input ? input.title.toLowerCase().replace(/\s+/g, '-').slice(0, 200) : '';
+				},
+			},
+		},
+		{
+			title: 'Short Description',
+			name: 'shortDescription',
+			type: 'string',
+		},
+		{
+			title: 'Price',
+			name: 'price',
+			type: 'number',
+		},
+		{
+			title: 'Thumbnail',
+			name: 'thumbnail',
+			type: 'image',
+		},
+		//Download TODO: Add action type field that hides or show either PluginMessage field or link field.
+		//Download TODO: Add link field.
+		{
+			title: 'Form Addon Message',
+			name: 'pluginMessage',
+			type: 'string',
+		},
+		{ title: 'Priority', name: 'priority', type: 'number' },
+	],
+	preview: {
+		select: {
+			title: 'title',
+		},
+		prepare(selection) {
+			return {
+				title: `Product - "${!selection.title ? 'Unset' : selection.title}"`,
+			};
+		},
+	},
 };
