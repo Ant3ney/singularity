afd8424eacd7333ba4a2f6ece6b8d1ea45a66c9c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2026-01-06 21:45:21 -0800

    Change placeholder copy to real copy

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const CallToAction = () => {
+	//                                <img className="cta-two__icon dimon-icon-data" src="/assets/images/cta2_icon_01.webp"/>
+		//                                                                <i className="cta-one__icon dimon-icon-data1"></i>
         return (
             <section className="cta-one">
                 <img src="/assets/images/background/cta-one-bg.png" className="cta-one__bg" alt="Awesome Image" />
@@ -9,25 +11,22 @@ const CallToAction = () => {
                     <div className="row justify-content-lg-end">
                         <div className="col-lg-6">
                             <div className="cta-one__content">
-                                <i className="cta-one__icon dimon-icon-data1"></i>
+				<img className="cta-two__icon dimon-icon-data" src="/assets/images/cta2_icon_01.webp"/>
                                 <div className="block-title text-left">
-                                    <h2 className="block-title__title">Powerful <span>Tools</span> for Your Customers.
+                                    <h2 className="block-title__title">Design Is <span>King</span>. Quality Above All.
                                     </h2>
                                 </div>
                                 <div className="cta-one__text">
-                                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                                        fugiat nulla pariatur.</p>
+                                    <p>We obsess over visuals, clarity, and craft to build striking websites that stand apart and run clean.</p>
                                 </div>
                                 <ul className="list-unstyled">
-                                    <li><i className="fa fa-check"></i>Refresing to get such a personal touch.</li>
-                                    <li><i className="fa fa-check"></i>Duis aute irure dolor in reprehenderit in
-                                        voluptate.
+                                    <li><i className="fa fa-check"></i>Built for brands that refuse to look average</li>
+                                    <li><i className="fa fa-check"></i>Comfortable where most developers back out
                                     </li>
-                                    <li><i className="fa fa-check"></i>Velit esse cillum dolore eu fugiat nulla
-                                        pariatur.
+                                    <li><i className="fa fa-check"></i>We lead with visuals, not templates.
                                     </li>
                                 </ul>
-                                <a href="#" className="thm-btn"><span>Get Started</span></a>
+                                <a href="#contact_us_singularity" className="thm-btn"><span>Get Started</span></a>
                             </div>
                         </div>
                     </div>
